8bde6d539f7ab52712f372008d95e68863835608
keshavanarayan <74804559+keshavanarayan@users.noreply.github.com>
2021-08-29 16:57:45 +0530

    changed gh file name

--- src/examples/App/script.js
+++ src/examples/App/script.js
@@ -9,7 +9,7 @@ import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm
 const loader = new Rhino3dmLoader()
 loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
-const definition = 'metaballTable.gh'
+const definition = 'Remap+Ranking_Internalised.gh'
 
 // setup input change events
 const distance_slider = document.getElementById( 'rangeValue' )
